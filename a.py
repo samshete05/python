a6d63d4ca32c9a782c0f70b0249928dccdacfeb4
samshete05 <samshete2005@gmail.com>
2023-09-01 15:26:00 +0530

    szv

---
+++ a.py
@@ -0,0 +1,30 @@
+# for i in range(10,2,-1):
+#     # print(5-i)
+#     for s in range (i):
+#         print('*',end='')
+#     print("")
+  
+# x= int(input ("Enter the value of x"))
+
+
+# y= int(input ("Enter the value of y"))
+
+# z=x+y
+# print (z)
+
+s=4
+o=5
+if s<o :
+    print('true')
+
+elif s>o :
+   print('False')
+
+else :
+    print('hola')
+   
+   
+
+
+
+
